3771e542bed85b9dc79ec06472e5a503f45055be
Angeth-Herjok <angethherjok@gmail.com>
2023-03-10 19:09:58 +0300

    first commit

---
+++ string.js
@@ -0,0 +1,20 @@
+let text="Beautiful"
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text[3]);
+
+const food="I did not have apetite today"
+
+console.log("I did not h"+"eat ave apetite today")
+
+const story="She sell sea shells at the see show"
+console.log()
+
+let b="CONFIdent";
+let b1=b.Uppercase();
+console.log(b.Uppercase());
+
+let y="amazing";
+let 
+
